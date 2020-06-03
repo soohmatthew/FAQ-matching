0231c6c5025bf70c14d4bbe9922c84b9ac98c263
Loh Zheng Yi <starlight@Lohs-MacBook-Pro.local>
2020-06-03 12:46:30 +0800

    Updated setup file

--- setup.sh
+++ setup.sh
@@ -1,34 +1,31 @@
 #!/bin/sh
-# if ! type pip > /dev/null; then
-#     echo "Installing pip"
-#     curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-#     python get-pip.py
-# fi
+echo "Update Packages"
+apt-get update
+apt-get upgrade
+apt install python3-pip
+apt install python3-venv
 
-# echo "Installing venv"
-# pip3 install --upgrade pip
-# pip3 install virtualenv
+echo "Upgrading Pip"
+pip3 install --upgrade pip
 
-# apt-get install python3-venv
-
-# echo "Creating Virtual Env"
-# python -m venv .env
-# source .env/bin/activate
+echo "Creating Virtual Env"
+python3 -m venv .asag_env
+source .asag_env/bin/activate
 
 echo "Installing Basic Conda Packages"
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
-echo "Installing Gensim"
-pip install --upgrade gensim
+# echo "Installing Gensim"
+# pip install --upgrade gensim
 
-echo "Installing Torch"
-pip install torch torchvision
+# echo "Installing Torch"
+# pip install torch torchvision
 
 echo "Installing Spacy"
-pip install -U spacy
+pip3 install -U spacy
 
 echo "Downloading Spacy Corpus"
-python -m spacy download en_core_web_sm
+python3 -m spacy download en_core_web_sm
 
 echo "Building neuralcoref from source"
 cd ..
@@ -36,14 +33,12 @@ git clone https://github.com/huggingface/neuralcoref.git
 cd neuralcoref
 pip install -e .
 cd ..
-cd GIM_ASAG
+cd gim_asag
 
 echo "Downloading word2vec vectors"
 wget http://wikipedia2vec.s3.amazonaws.com/models/en/2018-04-20/enwiki_20180420_100d.txt.bz2 -P ./Resource/
-bzip2 enwiki_20180420_100d.txt.bz2 -d ./Resource/
-rm ./Resource/enwiki_20180420_100d.txt.bz2
+bzip2 -d ./Resource/enwiki_20180420_100d.txt.bz2
 
 echo "Downloading fastText vectors"
 wget https://dl.fbaipublicfiles.com/fasttext/vectors-crawl/cc.en.300.bin.gz -P ./Resource/
-gunzip cc.en.300.bin.gz -d ./Resource/
-rm ./Resource/cc.en.300.bin.gz
+gunzip -d ./Resource/cc.en.300.bin.gz
